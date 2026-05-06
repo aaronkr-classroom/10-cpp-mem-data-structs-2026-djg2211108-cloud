878e2efab2f03d841db3beee2d2bdb2e73721905
ththfql <145094239+ththfql@users.noreply.github.com>
2026-05-06 17:08:13 +0900

    ㅎㅎ

---
+++ Project1/copy_file.cpp
@@ -0,0 +1,15 @@
+﻿#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	ifstream infile("in.txt");//지금 만들어야 한다
+	ofstream outfile("out.txt");//지금 없다
+
+	string s;
+
+	while (getline(infile, s))
+		outfile << s << endl; 
+	return 0;
+}
